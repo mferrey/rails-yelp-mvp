aea8b75ce82842824365bcb2ce72506de6b616b4
mathilde ferrey <mathildeferrey@yahoo.com>
2019-02-13 16:58:15 +0100

    deuxième exo OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+# root to: 'controller#index'
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'restaurants#index'
+  resources :restaurants do
+    resources :reviews, only: [:create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(name: 'Italia', address: 'Roma', phone_number: "01 33 33 33 33", category: 'italian')
+Restaurant.create(name: 'Belgium', address: 'Bruxelles', phone_number: '01 33 33 33 33', category: 'belgian')
+Restaurant.create(name: 'China', address: 'Beijing', phone_number: '01 33 33 33 33', category: 'chinese')
+Restaurant.create(name: 'French', address: 'Paris', phone_number: '01 33 33 33 33', category: 'french')
+Restaurant.create(name: 'Japanese', address: 'Kyoto', phone_number: '01 33 33 33 33', category: 'japanese')
